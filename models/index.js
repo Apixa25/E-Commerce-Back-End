955909e7367f4835db01d2445825296f99272abc
stevensillsii <stevensills2@gmail.com>
2023-11-03 19:22:37 -0700

    defined the column and setup the fields in product.js

--- models/index.js
+++ models/index.js
@@ -14,7 +14,7 @@ Category.hasMany(Product, {
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: PrdoductTag,
+  through: ProductTag,
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
